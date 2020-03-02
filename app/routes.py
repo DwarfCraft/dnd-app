788bb3017ea72b8fb83504bdda9f1e3385be34a7
Adam Holmes <adam.holmes@bcbssc.com>
2020-03-02 13:32:08 -0500

    template testing

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,15 @@ from app import app
 
 @app.route('/')
 @app.route('/index')
+@app.route('/home')
 def index():
     user = {'username': 'Miguel'}
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/template')
+def home():
     return render_template('template.html')
